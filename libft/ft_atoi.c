6ed646814d1c6694fa06bfdc7af1ba04ba9103c8
Ironesto <gabriel.p.g@hotmail.es>
2023-08-31 03:15:38 +0200

    02

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -23,19 +23,19 @@ int ft_atoi(const char *nptr)
     i = 0;
     while (nptr[i] <= 32)
         i++;
-    if (nptr[i] == '+' && nptr[i] >='0' && nptr[i] <= '9')
+    if (nptr[i] == '+' && nptr[i + 1] >='0' && nptr[i + 1] <= '9')
         return(ft_putnbr(nptr, i + 1));
-    if (nptr[i] == '-' && nptr[i] >='0' && nptr[i] <= '9')
+    if (nptr[i] == '-' && nptr[i + 1] >='0' && nptr[i + 1] <= '9')
         return(ft_putnbr(nptr, i + 1) * -1);
     if (nptr[i] >='0' && nptr[i] <= '9')
         return(ft_putnbr(nptr, i));
     return (0);
 }
 
-int main()
+/*int main()
 {
-    char str[] = "\t \n   + 123a mundo 34";
+    char str[] = "\t \n   - 12367a mundo 34";
     printf("%d", ft_atoi(str));
-    printf("%d", atoi(str));
+    printf("\n%d", atoi(str));
     return (0);
-}
+}*/
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-    
+    return (s1);
+}
+
+int main (void)
+{
+    printf("%s", ft_strtrim("hola mundo", "la"));
+    return (0);
 }
