079d1e107e2cc967c3129ad9365136ff50337605
Ironesto <gabriel.p.g@hotmail.es>
2023-09-01 03:57:16 +0200

    corr

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -7,6 +7,13 @@ int ft_ext(int num)
     int i;
 
     i = 0;
+    if (num == 0)
+        i = 1;
+    if (num < 0)
+    {
+        num = -num;
+        i++;
+    }
     while (num > 0)
     {
         num /= 10;
@@ -17,6 +24,8 @@ int ft_ext(int num)
 
 char *ft_putnbr(char *str, int i, int num)
 {
+    if (num < 0)
+        num = -num;
     str[i] = (num % 10) + '0';
     if (i > 0)
         ft_putnbr(str, i - 1, num / 10);
@@ -29,14 +38,17 @@ char *ft_itoa(int n)
     int j;
 
     j = ft_ext(n);
-    res = malloc(sizeof(char) * j);
+    res = malloc(sizeof(char) * j + 1);
     ft_putnbr(res, j - 1, n);
+    res[j + 1] = '\0';
+    if (n < 0)
+        res[0] = '-';
     return (res);
 }
 
 int main (void)
 {
-    printf("%s",ft_itoa(12346));
+    printf("%s",ft_itoa(102));
 
     return(0);
 }
