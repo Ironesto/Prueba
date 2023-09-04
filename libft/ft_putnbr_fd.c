a19b6bea9a5c05e283a3404430093984c8b6a9d2
Ironesto <gabriel.p.g@hotmail.es>
2023-09-04 03:37:04 +0200

    .txts

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -15,10 +15,10 @@ void ft_putnbr_fd(int n, int fd)
     write (fd,&c,1);
 }
 
-int main (void)
+/*int main (void)
 {
     int fd;
     char str[] = "hola mundo \n que tal.";
     fd = open("/home/ironesto/Escritorio/Cursus/libft/file.txt", O_WRONLY);
     ft_putnbr_fd(-123, fd);
-}
+}*/
