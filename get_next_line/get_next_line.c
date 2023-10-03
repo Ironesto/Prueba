afe850de43a43439966174b5fb0fa1cf9265c098
ironesto <gabriel.p.g@hotmail.es>
2023-10-03 13:56:47 +0200

    02

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,15 +12,77 @@
 
 #include "get_next_line.h"
 
+
+char *first_part(char *str, int len)
+{
+	int		i;
+	char	*res;
+
+	i = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[i] && str[i] != '\n' && i < BUFFER_SIZE)
+		i++;
+	res = malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i] && str[i] != '\n' && i < BUFFER_SIZE)
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char *last_part(char *str, int len)
+{
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	i++;
+	j = i;
+	while (str[i])
+		i++;
+	res = malloc(sizeof(char) * (i - j + 1));
+	i = 0;
+	while (str[j])
+	{
+		res[i] = str[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
 char *get_next_line(int fd)
 {
-	static char *buffer;
+	static char	*buffer;
+	char		*res;
+	int			j;
 
+	j = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	read(fd, buffer, BUFFER_SIZE);
-	return (buffer);
+	j = read(fd, buffer, BUFFER_SIZE);
+	while (j > 0)
+	{	
+		while (buffer[0] != '\0')
+		{
+			res = first_part(buffer, BUFFER_SIZE);
+			buffer = last_part(buffer, BUFFER_SIZE);
+		}
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		j = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (res);
 }
 
 int main()
@@ -28,7 +90,20 @@ int main()
 	int	fd;
 
 	fd = open("./prueba.txt", O_RDONLY);
-	printf("%d\n", fd);
-	printf("%s", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
 	return(0);
 }
+
+/*char *get_next_line(int fd)
+{
+	static char *buffer;
+	char *res;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	read(fd, buffer, BUFFER_SIZE);
+	res = first_part(buffer, BUFFER_SIZE);
+	buffer = last_part(buffer, BUFFER_SIZE);
+	return (res);
+}*/
