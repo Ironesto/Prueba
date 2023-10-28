c62ec2d3c9346d339b4e990091a288d2b33c1ffb
Ironesto <gabriel.p.g@hotmail.es>
2023-10-28 05:08:57 +0200

    push_swap

--- push_swap/moves.c
+++ push_swap/moves.c
@@ -12,6 +12,8 @@ void	swap(int *p)
 {
 	int	aux;
 
+	if(!p[1] || !p[0])
+		return ;
 	aux = p[0];
 	p[0] = p[1];
 	p[1] = aux;
@@ -30,6 +32,8 @@ void	push(int *p1, int *p2)
 	int	i;
 	int	k;
 
+	if (p2[0] == 0 && p2[1] == 0)
+		return ;
 	aux = 0;
 	i = p1[aux];
 	while(p1[aux])
@@ -40,8 +44,16 @@ void	push(int *p1, int *p2)
 		aux++;
 	}
 	p1[0] = p2[0];
-
-	//mover arriba el resto de nums de p2 y comprobar lo primero si p2 esta vacio
+	aux = 0;
+	while (p2[aux])
+		aux++;
+	while(p2[aux - 3])
+	{
+		k = p2[aux - 1];
+		p2[aux - 1] = i;
+		i = k;
+		aux--;
+	}
 }
 /*
 ra rotate a: Desplaza hacia arriba todos los elementos del stack a una posición,
@@ -51,7 +63,25 @@ rb rotate b: Desplaza hacia arriba todos los elementos del stack b una posición
 de forma que el primer elemento se convierte en el último.
 
 rr ra y rb al mismo tiempo.
+*/
+void	rotate(int *p)
+{
+	int aux;
+	int	i;
+	int	k;
 
+	aux = 0;
+	while (p[aux])
+		aux++;
+	while(p[aux - 3])
+	{
+		k = p[aux - 1];
+		p[aux - 1] = i;
+		i = k;
+		aux--;
+	}
+}
+/*
 rra reverse rotate a: Desplaza hacia abajo todos los elementos del stack a una
 posición, de forma que el último elemento se convierte en el primero.
 
@@ -62,12 +92,12 @@ rrr rra y rrb al mismo tiempo. */
 int	main(int argc, char **argv)
 {
 	int	*stack_a;
-	int	*stack_b;
+	int	stack_b[10] = {16, 23, 55, 79, 101, 43, 88, 454};
 	int	i;
 	int	aux = 0;
 
-	stack_a = malloc(sizeof(int) * argc);
-	stack_b = malloc(sizeof(int) * argc);
+	stack_a = malloc(sizeof(int) * (argc + 1));
+	//stack_b = malloc(sizeof(int) * argc);
 	while (i < argc - 1)
 	{
 		stack_a[i] = atoi(argv[i + 1]);
@@ -76,10 +106,10 @@ int	main(int argc, char **argv)
 	push(stack_a, stack_b);
 	while (aux < argc)
 	{
-		printf("%d\n", stack_a[aux]);
+		printf("%d\n", stack_b[aux]);
 		aux++;
 	}
 	free(stack_a);
-	free(stack_b);
+	//free(stack_b);
 	return(0);
 }
